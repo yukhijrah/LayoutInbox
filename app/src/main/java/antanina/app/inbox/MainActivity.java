d5a63b5142b73cb4a9bd7d4453ca410e7ba71120
anifatulaufah <anifatulaufah09@gmail.com>
2018-11-02 08:55:03 +0700

    Tugas Layout Inbox Anifatul Annisa

---
+++ app/src/main/java/antanina/app/inbox/MainActivity.java
@@ -0,0 +1,33 @@
+package antanina.app.inbox;
+
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView ListChat;
+    LinearLayoutManager linear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.cardview_item);
+    }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.cardview_item2);
+    }
+}
